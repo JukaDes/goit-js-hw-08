095f7365a8f1d38ece526e93e65908d77ab91eb5
Jekaterina Sedjuka <jekaterina.sedjuka@gmail.com>
2022-10-07 07:39:26 +0100

    finished video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(videoCurrentTime, 1000));
+function videoCurrentTime (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds)
+}
+
+const timeUpdate = Number(localStorage.getItem('videoplayer-current-time'))
+
+player.setCurrentTime(timeUpdate)
+    .then(function (seconds) {
+    })
+    .catch(function (error) {
+    switch (error.name) {
+        case 'RangeError': break;
+        default: break;
+    }
+});
